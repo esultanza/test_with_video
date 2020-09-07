42bb462324f9e78d4ec4bc8afa03387b944a2199
svasenkov <aanher@gmail.com>
2020-09-07 20:09:46 +0300

    first commit, added yandex search test

---
+++ src/test/java/tests/BrowserTests.java
@@ -0,0 +1,32 @@
+package tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+@Epic("QA.GURU automation course")
+@Story("Google tests with video")
+@Tag("google")
+class BrowserTests {
+
+    @Test
+    @Description("Google test, we look for wikipedia")
+    @DisplayName("Successful search for wikipedia in google")
+    void successfulSearch() {
+        open("http://ya.ru");
+
+        $("#text").val("wikipedia").pressEnter();
+
+        $("html").shouldHave(text("wikipedia.org"));
+    }
+
+}
